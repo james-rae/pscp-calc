d6e2b6e6e61f105189c202b8071e7b257b96bd74
James Rae <james.b.rae@gmail.com>
2025-07-03 07:57:26 -0400

    enter masher

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,20 @@ enhancer.onclick = buildGrid;
 
 const exporter = Domb.git('cmdExport');
 exporter.onclick = exportGrid;
+
+// refresh grid if user mashes enter when inside an input box
+
+const enterEnhancer = (e: KeyboardEvent): void => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+
+        buildGrid();
+    }
+};
+
+const inputEnterFields = ['cpp', 'oas', 'pension', 'empStart', 'empEnd', 'salary'];
+
+inputEnterFields.forEach(fieldId => {
+    const field = Domb.git(fieldId) as HTMLInputElement;
+    field.onkeyup = enterEnhancer;
+});
